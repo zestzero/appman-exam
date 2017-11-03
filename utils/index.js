c0386b7167f9f59a498651f89dafd7785f665ef6
Bannachaisirisuk, Krit (Agoda) <krit.bannchaisirisuk@agoda.com>
2017-11-03 17:57:50 +0700

    Add answer for lodash part

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,26 @@ const _ = require('lodash');
 
 // WARNING: beware object mutable
 
+function convertFormat({ name, scores }) {
+    return _.map(scores, (item, key, index) => {
+        return {
+            subject: key,
+            students: [{ name: name, score: item }]
+        };
+    });
+}
+
+function transformData(store) {
+    return _.reduce(store, (acc, subject) => {
+        return _.merge({}, acc, _.map(subject.students, (student) => {
+            return {
+                name: student.name,
+                [subject.subject]: student.score
+            };
+        }));
+    }, {});
+}
+
 /**
  * @params {Object} store
  * @params {String} name
@@ -10,6 +30,17 @@ const _ = require('lodash');
  */
 exports.updateStudentScore = (store, { name, scores }) => {
     // code here
+    const convertedObj = convertFormat({ name, scores });
+    const getMatchedSubject = (subject) => { return _.find(convertedObj, ['subject', subject]); };
+
+    const result = _.map(store, ({ subject, students }) => {
+        return {
+            subject: subject,
+            students: _.union([], students, _.get(getMatchedSubject(subject), 'students', [])),
+        };
+    });
+
+    return result;
 };
 
 /**
@@ -18,12 +49,26 @@ exports.updateStudentScore = (store, { name, scores }) => {
  * @params {String} subject
  */
 exports.removeStudentScoreBySubject = (store, { name, subject }) => {
-    // code here
+    const getRemovedStudent = (students, name) => {
+        return _.filter(students, (s) => { return s.name !== name; });
+    };
+
+    const getResult = (store, name, subject) => {
+        return _.map(store, (s) => {
+            return {
+                subject: s.subject,
+                students: s.subject === subject ? getRemovedStudent(s.students, name) : s.students
+            };
+        });
+
+    };
+
+    return getResult(store, name, subject);
 };
 
 /**
  * @params {Object} store
  */
 exports.transformData = store => {
-    // code here
+    return _.map(transformData(store), (o) => o);
 };
